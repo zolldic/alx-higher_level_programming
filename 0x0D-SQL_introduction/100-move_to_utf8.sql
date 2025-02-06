20b34997730f9a0a0f11be642466dc3dbf2562ec
= <=>
2025-02-07 01:02:00 +0200

    feat: covert database, table and varchar field to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,13 @@
+-- convert a databse to UTF-8
+
+-- convert the database
+ALTER DATABASE hbtn_0c_0
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+
+
+-- convert first_table
+ALTER TABLE first_table
+	CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
+

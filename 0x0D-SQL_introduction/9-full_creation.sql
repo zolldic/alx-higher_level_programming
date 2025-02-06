f61c13f5c0a2a93fe15bbc6fc1f1ce74d0065943
= <=>
2025-02-06 23:56:54 +0200

    feat: create a database and insert multiple records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- create a table with multiples records
+
+
+-- create the table
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+	);
+
+-- insert the data
+INSERT INTO second_table (
+	id, name, score
+	) VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8)

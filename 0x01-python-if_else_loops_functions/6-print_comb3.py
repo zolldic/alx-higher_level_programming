cafb2f35080fceeecfbfbd30b6aefa6b47ff942f
zolldic <ojiip95@gmail.com>
2025-12-07 15:57:02 +0200

    style: fix pycodestyle issues

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 sep = ", "
-for x in range (0, 10):
+for x in range(0, 10):
     if x == 9:
         print()
-    for y in range (x, 10):
+    for y in range(x, 10):
         if x != y:
             if x == 8:
                 sep = ""

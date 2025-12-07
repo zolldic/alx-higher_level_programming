486000e8ee36bd358fed8915ab1369f9231b19a9
zolldic <ojiip95@gmail.com>
2025-12-07 16:13:16 +0200

    feat: A function that prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    new = []
+    for letter in str:
+        if ord(letter) <= 122 and ord(letter) >= 97:
+            new.append(chr(ord(letter) - 32))
+        else:
+            new.append(letter)
+    print("".join(new))

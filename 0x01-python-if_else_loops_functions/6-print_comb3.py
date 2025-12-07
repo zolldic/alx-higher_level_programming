19e3acd88db56c3aacb65f3e6b0c95ad0d69f658
zolldic <ojiip95@gmail.com>
2025-12-07 15:54:26 +0200

    feat: A program that prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+sep = ", "
+for x in range (0, 10):
+    if x == 9:
+        print()
+    for y in range (x, 10):
+        if x != y:
+            if x == 8:
+                sep = ""
+            print("{}{}{}".format(x, y, sep), end="")

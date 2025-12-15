95143ca978c1d1c38b653f4777deb37faffa6771
zolldic <ojiip95@gmail.com>
2025-12-15 20:02:12 +0200

    feat: Implement a function that prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0;
+
+    try:
+        for idx in range(x):
+            print("{}".format(my_list[idx]), end='')
+            count += 1
+    except IndexError:
+        pass
+
+    print()
+    return count
+
+
